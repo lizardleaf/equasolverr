77397d6fd205c61a047fa6db5e6e7410b7020ddc
lizardleaf <167785884+lizardleaf@users.noreply.github.com>
2024-05-23 17:00:46 +0100

    Update factors.js

--- factors.js
+++ factors.js
@@ -5,15 +5,29 @@ function calculate() {
     let b = 0;
     let c = 0;
     let d = [];
-    for (let i = 0; i < a; i++) {
-        for (let j = 0; j < a; j++) {
-            if (c*b == a) {
-                d.push(c);
+    if (a.includes("-") == true) {
+        for (let i = 0; i < (a*-1)+1; i++) {
+            for (let j = 0; j < (a*-1)+1; j++) {
+                if (c*b == (a*-1)) {
+                    d.push(`-${c}`);
+                }
+                b++;
             }
-            b++;
+            b = 0;
+            c++;
         }
-        b = 0;
-        c++;
+        output.innerText = d;
+    } else {
+        for (let i = 0; i < a+1; i++) {
+            for (let j = 0; j < a+1; j++) {
+                if (c*b == a) {
+                    d.push(c);
+                }
+                b++;
+            }
+            b = 0;
+            c++;
+        }
+        output.innerText = d;
     }
-    output.innerText = `1,${d},${a}`;
 }
